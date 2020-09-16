47b4dee2b721159710dbefc10eddd34e667d0f6d
Anna Schneider <anna.schneiderm@gmail.com>
2020-09-16 19:16:46 -0400

    Refined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :votes
-  resources :movies
-  resources :events
+  resources :votes, only: :create
+  resources :movies, only: :index
+  resources :events, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
