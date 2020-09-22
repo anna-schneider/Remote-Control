b47f6b75bdbd70715c10632396568dabfc30f5e0
Anna Schneider <anna.schneiderm@gmail.com>
2020-09-22 08:34:31 -0400

    Button styling/breakpoints on Home

--- client/src/components/shared/button/Button.jsx
+++ client/src/components/shared/button/Button.jsx
@@ -5,7 +5,7 @@ import "./Button.css"
 export default function Button(props) {
 	return (
 		<div>
-			<button className="button" onClick={props.onClick}>
+			<button className={`button ${props.className}`} onClick={props.onClick}>
 				{props.content}
 			</button>
 		</div>
